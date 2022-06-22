5b43965fbf7763014eb2326ee9fe7a867e2bb423
Yan Mingzhi <realraindust@gmail.com>
2022-06-22 17:00:30 +0800

    add hello world example

---
+++ hello-world/src/main.rs
@@ -0,0 +1,36 @@
+use wasmtime::{Caller, Engine, Func, Instance, Module, Store};
+
+fn main() -> anyhow::Result<()> {
+    let engine = Engine::default();
+    let module = Module::new(&engine, WAT)?;
+
+    // All wasm objects operate within the context of a "store". Each
+    // `Store` has a type parameter to store host-specific data, which in
+    // this case we're using `4` for.
+    let mut store = Store::new(&engine, 4);
+    let host_hello = Func::wrap(&mut store, |caller: Caller<'_, u32>, param: i32| {
+        println!("Got {} from WebAssembly", param);
+        println!("my host state is: {}", caller.data());
+    });
+
+    // Instantiation of a module requires specifying its imports and then
+    // afterwards we can fetch exports by name, as well as asserting the
+    // type signature of the function with `get_typed_func`.
+    let instance = Instance::new(&mut store, &module, &[host_hello.into()])?;
+    let hello = instance.get_typed_func::<(), (), _>(&mut store, "hello")?;
+
+    // And finally we can call the wasm!
+    hello.call(&mut store, ())?;
+
+    Ok(())
+}
+
+const WAT: &str = r#"
+        (module
+            (import "host" "hello" (func $host_hello (param i32)))
+
+            (func (export "hello")
+                i32.const 3
+                call $host_hello)
+        )
+    "#;
