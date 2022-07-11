b6063446fef86de9717a02fd635771d1a77f9b9b
Yan Mingzhi <realraindust@gmail.com>
2022-07-11 22:50:51 +0800

    add example about wasi with wbindgen

---
+++ wasi-with-wbindgen/wasi-with-wbindgen-example/src/lib.rs
@@ -0,0 +1,26 @@
+use gluesql_core::prelude::{Payload, Value};
+
+#[no_mangle]
+pub extern "C" fn __guest_call(key: i32) -> u32 {
+    // println can't work in wasm
+    println!("call from guest (web assemply)");
+    println!("passing key is {}", key);
+    unsafe { __passing_key(key as u32) }
+    println!("end of call from guest (web assemply)");
+    unsafe { __host_call() }
+
+    let payload = Payload::Create;
+    println!("payload: {:?}", payload);
+    let value = Value::Bool(true);
+    println!("value: {:?}", value);
+
+    key as u32 * 2
+}
+
+// note import module should have no returns
+#[link(wasm_import_module = "wapc")]
+extern "C" {
+    pub fn __host_call();
+
+    pub fn __passing_key(key: u32);
+}
